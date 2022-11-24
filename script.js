bd94ded101789e4729eadbfdded56685dfd1c692
Aleksandr <alexandr.falkone@yandex.ru>
2022-11-24 23:23:33 +0300

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ const account1 = {
   transactions: [500, 250, -300, 5000, -850, -110, -170, 1100],
   interest: 1.5,
   pin: 1111,
+  transactionsDates: [
+    "2020-10-02T14:43:31.074Z",
+    "2020-10-29T11:24:19.761Z",
+    "2020-11-15T10:45:23.907Z",
+    "2021-01-22T12:17:46.255Z",
+    "2021-02-12T15:14:06.486Z",
+    "2021-03-09T11:42:26.371Z",
+    "2021-05-21T07:43:59.331Z",
+    "2021-06-22T15:21:20.814Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const account2 = {
@@ -14,6 +26,18 @@ const account2 = {
   transactions: [2000, 6400, -1350, -70, -210, -2000, 5500, -30],
   interest: 1.3,
   pin: 2222,
+  transactionsDates: [
+    "2020-10-02T14:43:31.074Z",
+    "2020-10-29T11:24:19.761Z",
+    "2020-11-15T10:45:23.907Z",
+    "2021-01-22T12:17:46.255Z",
+    "2021-02-12T15:14:06.486Z",
+    "2021-03-09T11:42:26.371Z",
+    "2021-05-21T07:43:59.331Z",
+    "2021-06-22T15:21:20.814Z",
+  ],
+  currency: "UAH",
+  locale: "uk-UA",
 };
 
 const account3 = {
@@ -21,6 +45,18 @@ const account3 = {
   transactions: [900, -200, 280, 300, -200, 150, 1400, -400],
   interest: 0.8,
   pin: 3333,
+  transactionsDates: [
+    "2020-10-02T14:43:31.074Z",
+    "2020-10-29T11:24:19.761Z",
+    "2020-11-15T10:45:23.907Z",
+    "2021-01-22T12:17:46.255Z",
+    "2021-02-12T15:14:06.486Z",
+    "2021-03-09T11:42:26.371Z",
+    "2021-05-21T07:43:59.331Z",
+    "2021-06-22T15:21:20.814Z",
+  ],
+  currency: "RUB",
+  locale: "ru-RU",
 };
 
 const account4 = {
@@ -28,6 +64,15 @@ const account4 = {
   transactions: [530, 1300, 500, 40, 190],
   interest: 1,
   pin: 4444,
+  transactionsDates: [
+    "2020-10-02T14:43:31.074Z",
+    "2020-10-29T11:24:19.761Z",
+    "2020-11-15T10:45:23.907Z",
+    "2021-01-22T12:17:46.255Z",
+    "2021-02-12T15:14:06.486Z",
+  ],
+  currency: "EUR",
+  locale: "fr-CA",
 };
 
 const account5 = {
@@ -35,6 +80,15 @@ const account5 = {
   transactions: [630, 800, 300, 50, 120],
   interest: 1.1,
   pin: 5555,
+  transactionsDates: [
+    "2020-10-02T14:43:31.074Z",
+    "2020-10-29T11:24:19.761Z",
+    "2020-11-15T10:45:23.907Z",
+    "2021-01-22T12:17:46.255Z",
+    "2021-02-12T15:14:06.486Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const accounts = [account1, account2, account3, account4, account5];
@@ -69,31 +123,36 @@ const inputClosePin = document.querySelector(".form__input--pin");
 
 // Показать транзакции
 
-const displayTransactions = function (transactions, sort = false) {
+const displayTransactions = function (account, sort = false) {
   containerTransactions.innerHTML = "";
 
   // Сортировать транзакции
 
   const transacs = sort
-    ? transactions.slice().sort((x, y) => x - y)
-    : transactions;
+    ? account.transactions.slice().sort((x, y) => x - y)
+    : account.transactions;
 
   transacs.forEach(function (trans, index) {
     const transType = trans > 0 ? "deposit" : "withdrawal";
+
+    const date = new Date(account.transactionsDates[index]);
+    const day = `${date.getDate()}`.padStart(2, "0");
+    const month = `${date.getMonth() + 1}`.padStart(2, "0");
+    const year = date.getFullYear();
+    const transDate = `${day}/${month}/${year}`;
 
     const transactionRow = `
     <div class="transactions__row">
           <div class="transactions__type transactions__type--${transType}">
             ${index + 1} ${transType}
           </div>
-          <div class="transactions__date">2 дня назад</div>
-          <div class="transactions__value">${trans}$</div>
+          <div class="transactions__date">${transDate}</div>
+          <div class="transactions__value">${trans.toFixed(2)}$</div>
         </div>
     `;
     containerTransactions.insertAdjacentHTML("afterbegin", transactionRow);
   });
 };
-displayTransactions(account1.transactions);
 
 // Добавить никнейм в аккаунт
 
@@ -114,44 +173,41 @@ createNicknames(accounts);
 const displayBalance = function (account) {
   const balance = account.transactions.reduce((acc, trans) => acc + trans, 0);
   account.balance = balance;
-  labelBalance.textContent = `${balance}$`;
+  labelBalance.textContent = `${balance.toFixed(2)}$`;
 };
-// displayBalance(account1.transactions);
 
 const displayTotal = function (account) {
   const depositesTotal = account.transactions
     .filter((trans) => trans > 0)
     .reduce((acc, trans) => acc + trans, 0);
-  labelSumIn.textContent = `${depositesTotal}$`;
+  labelSumIn.textContent = `${depositesTotal.toFixed(2)}$`;
 
   const withdrawalsTotal = account.transactions
     .filter((trans) => trans < 0)
     .reduce((acc, trans) => acc + trans, 0);
-  labelSumOut.textContent = `${withdrawalsTotal}$`;
+  labelSumOut.textContent = `${withdrawalsTotal.toFixed(2)}$`;
 
   const interestTotal = account.transactions
     .filter((trans) => trans > 0)
     .map((depos) => (depos * account.interest) / 100)
     .filter((interest) => interest >= 5)
     .reduce((acc, interest) => acc + interest, 0);
-  labelSumInterest.textContent = `${interestTotal}$`;
+  labelSumInterest.textContent = `${interestTotal.toFixed(2)}$`;
 };
 
-const updateUi = function (currentAccount) {
+const updateUi = function (account) {
   // Отобразить транзакции
 
-  displayTransactions(currentAccount.transactions);
+  displayTransactions(account);
 
   // Отобразить баланс
 
-  displayBalance(currentAccount);
+  displayBalance(account);
 
   // Отобразить итого
 
-  displayTotal(currentAccount);
+  displayTotal(account);
 };
-
-// displayTotal(account1.transactions);
 
 let currentAccount;
 
@@ -171,6 +227,12 @@ btnLogin.addEventListener("click", function (e) {
     labelWelcome.textContent = `Добро пожаловать, ${
       currentAccount.userName.split(" ")[0]
     }!`;
+
+    const nowDate = new Date();
+    const day = `${nowDate.getDate()}`.padStart(2, "0");
+    const month = `${nowDate.getMonth() + 1}`.padStart(2, "0");
+    const year = nowDate.getFullYear();
+    labelDate.textContent = `${day}/${month}/${year}`;
 
     // Очистить ввод
 
@@ -198,6 +260,8 @@ btnTransfer.addEventListener("click", function (e) {
   inputTransferAmount.value = "";
   inputTransferTo.value = "";
 
+  // Добавть транзакции
+
   if (
     transferAmount > 0 &&
     transferAmount <= currentAccount.balance &&
@@ -206,6 +270,12 @@ btnTransfer.addEventListener("click", function (e) {
   ) {
     currentAccount.transactions.push(-transferAmount);
     recipienAccount.transactions.push(transferAmount);
+
+    // Добавить дату транзакции
+
+    currentAccount.transactionsDates.push(new Date());
+    recipienAccount.transactionsDates.push(new Date());
+
     updateUi(currentAccount);
   }
 });
@@ -214,7 +284,7 @@ btnTransfer.addEventListener("click", function (e) {
 
 btnLoan.addEventListener("click", function (e) {
   e.preventDefault();
-  const loanAmount = Number(inputLoanAmount.value);
+  const loanAmount = Math.floor(Number(inputLoanAmount.value));
 
   if (
     loanAmount > 0 &&
@@ -223,6 +293,7 @@ btnLoan.addEventListener("click", function (e) {
     )
   ) {
     currentAccount.transactions.push(loanAmount);
+    currentAccount.transactionsDates.push(new Date());
     updateUi(currentAccount);
   }
   inputLoanAmount.value = "";
@@ -255,6 +326,6 @@ let transactionsSorted = false;
 
 btnSort.addEventListener("click", function (e) {
   e.preventDefault();
-  displayTransactions(currentAccount.transactions, !transactionsSorted);
+  displayTransactions(currentAccount, !transactionsSorted);
   transactionsSorted = !transactionsSorted;
 });
